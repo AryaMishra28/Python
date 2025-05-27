b5aac81fab6abed85b2659df9b6aaaa36d5af411
AryaMishra28 <Aryamishra2831@gmail.com>
2025-05-27 22:53:26 +0530

    Add files via upload

---
+++ Condition.py
@@ -0,0 +1,5 @@
+a=int(input("Enter your age"))
+if a>=18:
+    print("You can vote")
+else:
+    print("You are not alowed to vote ha ha ha ha")
---
+++ Condition2.py
@@ -0,0 +1,7 @@
+a=int(input("Enter your number"))
+if a<0:
+   print("This is a Negetive Number") 
+elif a==0:
+    print("This is a Netural Number")
+else:
+    print("this is a Positive Number")
