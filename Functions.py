d420a14a8bd6eab9e009a4274be57a13c760d459
AryaMishra28 <Aryamishra2831@gmail.com>
2025-06-03 22:43:43 +0530

    Add files via upload

---
+++ Functions.py
@@ -0,0 +1,17 @@
+def add():
+    a=656554334435
+    b=6547474544764646
+    print(a+b)
+add()
+
+
+def sub(a,b,c):
+    print(a-b-c)
+sub(1234,654,566)
+sub(6775656566446,67464,676767)
+
+a=int(input("enter your num"))
+b=int(input("enter your num"))
+def add():
+    print(a+b)
+add()
