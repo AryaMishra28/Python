9a44274cf9a8be2ae33e6aa64085d87a0634f33c
AryaMishra28 <Aryamishra2831@gmail.com>
2025-07-07 22:44:47 +0530

    Add files via upload

---
+++ FIZZ BUZZ .py
@@ -0,0 +1,12 @@
+for s in range(1,101):
+    if s%3==0 and s%5==0:
+        print("fizzbuzz")   
+    elif s%3==0:
+        print("fizz")
+    elif s%5==0:
+        print("buzz")
+    else:
+        print(s)
+
+
+
