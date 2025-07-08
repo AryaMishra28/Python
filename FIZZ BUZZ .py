9e9ec5cad80c26aef73167e71703d04f07cd0b84
AryaMishra28 <Aryamishra2831@gmail.com>
2025-07-08 22:13:07 +0530

    Add files via upload

--- FIZZ BUZZ .py
+++ FIZZ BUZZ .py
@@ -1,4 +1,6 @@
-for s in range(1,101):
+u=int(input("Enter your staring range"))
+a=int(input("Enter your ending range"))
+for s in range(u,a+1):
     if s%3==0 and s%5==0:
         print("fizzbuzz")   
     elif s%3==0:
