09f06c2bd9d9cde90f230c2c20e1a3a5c292f9b6
AryaMishra28 <Aryamishra2831@gmail.com>
2025-07-16 19:11:10 +0530

    Add files via upload

--- Guess the number.py
+++ Guess the number.py
@@ -1,8 +1,10 @@
 import random
 a=random .randint(1,10)
 s=int(input("Enter Number:"))
-if a==s:
-    print("Correct")
+if a<s:
+    print("TOO BIG maybe a little small num")
+elif a>s:
+    print("TOO LESS maybe a little big num")
 else:
-    print("Incorrect")
+    print("correct")
     print("Computer has selected",a)
